68cf58b7127b431a8c34e86715e40f82d3dedae9
windwithlife@hotmail.com <windwithlife@hotmail.com>
2019-02-12 19:32:24 +0800

    support remote access for  mysql via init_database.sql

---
+++ init_database.sql
@@ -0,0 +1,22 @@
+
+-- 允许远程mysql客户端访问：
+GRANT ALL ON *.* TO 'root'@'%';
+flush privileges;
+-- 切换数据库
+use mydb;
+
+-- 建表
+DROP TABLE IF EXISTS `table1`;
+
+CREATE TABLE `table1` (
+`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+`name` varchar(20) DEFAULT NULL COMMENT '姓名',
+`age` int(11) DEFAULT NULL COMMENT '年龄',
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 插入数据
+INSERT INTO `table1` (`id`, `name`, `age`)
+VALUES
+(1,'姓名1',10),
+(2,'姓名2',11);
